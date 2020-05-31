ec26453a0d0736adc8d607c9e4021f4b91788d35
Jimi Quintero <jjimiq@gmail.com>
2020-05-31 00:13:03 -0500

    Ventana Modal para Video

---
+++ script.js
@@ -0,0 +1,7 @@
+// Code goes here
+$(function(){
+    $('.modal').on('hidden.bs.modal', function (e) {
+      $iframe = $(this).find("iframe");
+      $iframe.attr("src", $iframe.attr("src"));
+    });
+  });
